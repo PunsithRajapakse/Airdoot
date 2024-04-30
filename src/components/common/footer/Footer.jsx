74c231385a8cf8ff5720311f28354a99c661ab37
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-04-30 11:46:20 +0530

    Footer completed

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,28 +1,46 @@
 import React from "react";
-import "./footer.css"; // Import your CSS file for styling
-import { FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa"; // Import relevant icons from react-icons library
+import "./footer.css";
+import {
+  FaEnvelope,
+  FaPhone,
+  FaMapMarkerAlt,
+  FaGooglePlay,
+  FaAppStoreIos,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="column">
+      <div className="column" style={{ width: "25%" }}>
         <div className="logo">
           <img src="Assets/alogo.png" alt="Logo" />
           <p>Best AC Repair And Services In India</p>
         </div>
         <div className="social-icons">
-          <a href="#" className="social-icon" target="_blank">
+          <a
+            href="https://www.facebook.com/airdoot.lk/"
+            className="social-icon"
+            target="_blank"
+          >
             <i className="fab fa-facebook" style={{ color: "#ffffff" }}></i>
           </a>
-          <a href="#" className="social-icon" target="_blank">
-            <i className="fab fa-linkedin" style={{ color: "#ffffff" }}></i>
-          </a>
-          <a href="#" className="social-icon" target="_blank">
+          <a
+            href="https://www.instagram.com/airdoot/"
+            className="social-icon"
+            target="_blank"
+          >
             <i className="fab fa-instagram" style={{ color: "#ffffff" }}></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/company/airdoot-india-pvt-ltd/?originalSubdomain=in"
+            className="social-icon"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin" style={{ color: "#ffffff" }}></i>
           </a>
         </div>
       </div>
-      <div className="column">
+      <div className="column" style={{ width: "15%" }}>
         <h3>Quick Links</h3>
         <ul>
           <li>
@@ -39,15 +57,18 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="column">
-        <h3>Sign up for our newsletter</h3>
+      <div className="column" style={{ width: "25%" }}>
+        <h3>Sign Up Our Newsletter</h3>
         <p>Stay up to update with our latest news and products</p>
         <div className="newsletter-form">
           <input type="email" placeholder="Enter your email" />
           <button>Submit</button>
         </div>
+        <div className="cservice">
+          <h3>Coming Soon Services</h3>
+        </div>
       </div>
-      <div className="column">
+      <div className="column" style={{ width: "25%" }}>
         <h3>Contact Us</h3>
         <div className="contact-info">
           <div className="contact-item">
@@ -74,16 +95,29 @@ const Footer = () => {
               <p>customercare@airdoot.com</p>
             </div>
           </div>
+          <div className="app">
+            <div className="icon">
+              <p>GET THE PARTNER APP</p>
+            </div>
+            <div className="store-icons">
+              <FaGooglePlay />
+              <FaAppStoreIos />
+            </div>
+          </div>
         </div>
       </div>
-      {/* <div className="additional-links">
+      <div className="additional-links" style={{ width: "100%" }}>
         <p>2024 © AirDoot Pte Ltd</p>
-        <p><a href="#">Privacy Policy</a></p>
-        <p><a href="#">Terms & Conditions</a></p>
-      </div> */}
+        <div className="vertical-line"></div>
+        <p>
+          <a href="">Privacy Policy</a>
+        </p>
+        <div className="vertical-line"></div>
+        <p>
+          <a href="">Terms & Conditions</a>
+        </p>
+      </div>
     </footer>
-    
-    
   );
 };
 
