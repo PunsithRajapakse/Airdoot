2d2f29e4797c54fa843e58ea433410be5072082d
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-04-30 18:01:43 +0530

    new

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Clients from "../../Components/Clients/Clients"; 
+import Clients from "../../Components/Clients/Clients";
 import Brands from "../../Components/Brands/Brands";
 
 const Home = () => {
   return (
-    <>
-    <div>
-      <h1>Welcome to the Home Page</h1>
-
-      <Brands/>
-      <Clients /> 
-
+    <div className="home">
+      <Brands />
+      <Clients />
     </div>
-    </>
   );
 };
 
