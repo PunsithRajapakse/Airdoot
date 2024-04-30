37906b73a4a052695cbcdd20606f05ca1ff4c9eb
Sasith Hirun <114715677+SasithHirun@users.noreply.github.com>
2024-04-30 17:36:40 +0530

    hj

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-import image1 from "./client1.png";
-import image2 from "./client2.png";
-import image3 from "./client3.png";
-import image4 from "./client4.png"
-import image5 from "./client5.png"
-import "./Clients.css"
+
+import "./Clients.css";
 
 const Clients = () => {
   // Array of brand logo image URLs
   const brandLogos = [
-    image1, 
-    image2,
-    image3,
-    image4,
-    image5,
-
+    process.env.PUBLIC_URL + "Assets/Clients/client1.png",
+    process.env.PUBLIC_URL + "Assets/Clients/client2.png",
+    process.env.PUBLIC_URL + "Assets/Clients/client3.png",
+    process.env.PUBLIC_URL + "Assets/Clients/client4.png",
+    process.env.PUBLIC_URL + "Assets/Clients/client5.png",
   ];
 
   return (
     <div className="clients">
-      <h1 style={{ color: "#FFE456" }}>Our Clients</h1><br/>
+      <h1 style={{ color: "#FFE456" }}>Our Clients</h1>
+      <br />
       <div className="our-clients-container">
-        
         {brandLogos.map((logo, index) => (
-          <img key={index} src={logo} alt={`Brand ${index + 1}`} className="brand-logoc" />
+          <img key={index} src={logo} alt={`Brand ${index + 1}`} className="brand-logo" />
         ))}
       </div>
     </div>
