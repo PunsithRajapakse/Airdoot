d71a6f1e4e0e0f6c984a05b43597b08a5f61221c
Sasith Hirun <114715677+SasithHirun@users.noreply.github.com>
2024-04-30 17:57:17 +0530

    brands added

---
+++ src/Components/Brands/Brands.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Brands.css"; // Import CSS file for styling (create this file if necessary)
+
+
+
+const Brands = () => {
+  // Array of brand logo image URLs
+  const brandLogos = [
+    process.env.PUBLIC_URL + "Assets/Brands/blue-star 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/carrier 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/daikin 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/general 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/hitachi 1 (1).png",
+    process.env.PUBLIC_URL + "Assets/Brands/lg 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/mcquay 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/hitachi 1 (1).png",
+    process.env.PUBLIC_URL + "Assets/Brands/samsung 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/toshiba 1.png",
+    process.env.PUBLIC_URL + "Assets/Brands/trane.png",
+    process.env.PUBLIC_URL + "Assets/Brands/voltas 1.png",
+    
+
+  ];
+
+  return (
+    <div className="brands">
+      <h1 style={{ color: "#FFE456" }}>We Service All Brands</h1><br/>
+      <div className="our-brands-container">
+        {/* Map through brandLogos array to render each brand logo */}
+        {brandLogos.map((logo, index) => (
+          <img key={index} src={logo} alt={`Brand ${index + 1}`} className="brand-logo" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Clients from "../../Components/Clients/Clients"; 
+import Brands from "../../Components/Brands/Brands";
 
 const Home = () => {
   return (
     <>
     <div>
       <h1>Welcome to the Home Page</h1>
+
+      <Brands/>
       <Clients /> 
+
     </div>
     </>
   );
