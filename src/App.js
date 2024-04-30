30e870ee98485628404da044acc460f382df4f12
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-04-30 17:13:33 +0530

    Home error fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
-// import Clients from "./components/clients/clients";
-import Home from "./Routes/Home";
+import Home from "./Routes/Home/Home";
 
 function App() {
   return (
     <Router>
-
-
       <div className="App">
-        <Route path="/"  component={Home}/>
-        <Route path="/footer" component={Footer} />
+        <Route path="/" component={Home} />
+        
       </div>
 
       <Footer />
--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Clients from "../Components/Clients/Clients";
+import Clients from "../../Components/Clients/Clients"; 
 
 const Home = () => {
   return (
+    <>
     <div>
       <h1>Welcome to the Home Page</h1>
-      {/* Add your home page content here */}
-
-      <Clients/>
+      <Clients /> 
     </div>
+    </>
   );
 };
 
