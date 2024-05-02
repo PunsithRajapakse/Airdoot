a6b98892488d4ff57b7a6633dc03f9f679d976e2
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-05-02 09:38:33 +0530

    Added why coose us section

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Clients from "../../Components/Clients/Clients";
 import Brands from "../../Components/Brands/Brands";
+import Choose from "../Choose/Choose";
 
 const Home = () => {
   return (
     <div className="home">
+      <Choose/>
       <Brands />
       <Clients />
     </div>
