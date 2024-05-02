787b18597aad09ff130f913a6a38bf240dd51f1d
Sasith Hirun <114715677+SasithHirun@users.noreply.github.com>
2024-05-02 11:59:29 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Routes/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <Router>
+
+      <Header/>
+      
       <div className="App">
         <Route path="/" component={Home} />
         
--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Clients from "../../Components/Clients/Clients";
 import Brands from "../../Components/Brands/Brands";
-import Choose from "../Choose/Choose";
+import Choose from "../../Components/Choose/Choose";
 
 const Home = () => {
   return (
