6e406142c35f35dd59a383d5ede43d25b1262517
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-05-28 09:41:22 +0530

    added new loading page

--- src/Components/Rollingpage/Rollingpage.jsx
+++ src/Components/Rollingpage/Rollingpage.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import './Rollingpage.css'; // Correct import path to your CSS file for styling
-import RollingpageImage from './Page.png'; // Correct import path to your image file
-
+import RollingpageVideo from './New.mp4'; // Correct import path to your video file
 
 const Rollingpage = ({ onFinish }) => {
     // Simulate a delay before finishing
     React.useEffect(() => {
-      const timer = setTimeout(() => {
-        onFinish();
-      }, 3000); // 3000 milliseconds delay, adjust as needed
-      return () => clearTimeout(timer);
+        const timer = setTimeout(() => {
+            onFinish();
+        }, 7000); // 3000 milliseconds delay, adjust as needed
+        return () => clearTimeout(timer);
     }, [onFinish]);
-  return (
-    <div className="Rollingpage">
-      <div className="background-image">
-        <img src={RollingpageImage} alt="Background" className="image-background" /> {/* Use imported image file */}
-      </div>
-    </div>
-  );
+
+    return (
+        <div className="Rollingpage">
+            <div className="background-video">
+                <video autoPlay muted loop className="video-background">
+                    <source src={RollingpageVideo} type="video/mp4" /> {/* Use imported video file */}
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    );
 };
 
 export default Rollingpage;
