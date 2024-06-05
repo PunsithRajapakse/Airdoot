6acfe0150f34577b5e0a4ecdc063186f43ac9e4b
Sasith Hirun <114715677+SasithHirun@users.noreply.github.com>
2024-06-05 13:49:44 +0530

    final

--- src/Components/NewApp/NewApp.jsx
+++ src/Components/NewApp/NewApp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NewApp.css"; // Import your CSS file for styling
+import arrowIcon from "./arrow.png"; // Import the arrow icon
 
 const NewApp = () => {
   return (
@@ -9,38 +10,38 @@ const NewApp = () => {
       <div className="container">
         <div className="box boxorange1">
           <span>Hospitality</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxblue2">
           <span>Healthcare</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxorange3">
           <span>Data Centers</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxtall boxblue4">
           <span>
             Supermarket & <br />
             Hypermarket
           </span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxtall boxblue5">
           <span>Residential</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxorange6">
           <span>Commercial Spaces</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxorange7">
           <span>Project Reports</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
         <div className="box boxtall boxorange8">
           <span>Commercial Kitchen</span>
-          <span className="arrow">➡</span>
+          <img src={arrowIcon} alt="Arrow" className="arrow" />
         </div>
       </div>
     </div>
