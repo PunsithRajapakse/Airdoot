bdc7537fd3e8c7fcffcc8306e49965a25eeb54d0
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2024-06-12 11:48:53 +0530

    Added tab and mac videos for loading page

--- src/Components/Rollingpage/Rollingpage.jsx
+++ src/Components/Rollingpage/Rollingpage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Rollingpage.css'; // Correct import path to your CSS file for styling
 import RollingpageVideoDesktop from './New.mp4'; // Correct import path to your desktop video file
 import RollingpageVideoMobile from './mobile.mp4'; // Correct import path to your mobile video file
+import RollingpageVideoTablet from './tab.mp4'; // Correct import path to your tablet video file
+import RollingpageVideoMacbook from './mac.mp4'; // Correct import path to your macbook video file
 
 const Rollingpage = ({ onFinish }) => {
     // Simulate a delay before finishing
@@ -21,6 +23,14 @@ const Rollingpage = ({ onFinish }) => {
                 </video>
                 <video autoPlay muted loop className="video-background mobile-video">
                     <source src={RollingpageVideoMobile} type="video/mp4" /> {/* Use imported mobile video file */}
+                    Your browser does not support the video tag.
+                </video>
+                <video autoPlay muted loop className="video-background tablet-video">
+                    <source src={RollingpageVideoTablet} type="video/mp4" /> {/* Use imported tablet video file */}
+                    Your browser does not support the video tag.
+                </video>
+                <video autoPlay muted loop className="video-background macbook-video">
+                    <source src={RollingpageVideoMacbook} type="video/mp4" /> {/* Use imported macbook video file */}
                     Your browser does not support the video tag.
                 </video>
             </div>
